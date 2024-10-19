472f74e001f327385b4661eee8a80e047a364816
Drumil Akhenia <86913953+drumil32@users.noreply.github.com>
2024-10-19 18:57:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Protshahan-Frontend/", // This should match your GitHub repository name
+  plugins: [react()]
 });
 
