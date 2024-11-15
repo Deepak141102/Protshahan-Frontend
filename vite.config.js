779a0300ba24d9ff6bf879f50487c15f74917263
Deepakgit config --global user.email d84255411@gamil.comgit config --global user.name Deepak <d84255411@gamil.com>
2024-11-15 13:55:51 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@fortawesome/fontawesome-svg-core', // Externalize FontAwesome
+        'chart.js', // Externalize chart.js if needed to avoid bundling
+      ]
+    }
+  },
 });
-
